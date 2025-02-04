9ac500f94d67d2eec2075efa0c1c9595feebd4e7
Kharisma Gilang <>
2025-02-04 10:41:46 +0700

    updating start.sh forgot to add ollama serve hehe

--- start.sh
+++ start.sh
@@ -5,9 +5,6 @@ set -e
 echo "Menjalankan ollama serve di background..."
 ollama serve > /dev/null 2>&1 &
 
-echo "Tunggu..."
-sleep 3
-
 # 2. Jalankan open-webui dengan uvx di background
 echo "Menjalankan open-webui di background..."
 DATA_DIR=$PWD/open-webui uvx --python 3.11 open-webui@latest serve --port 8090 > /dev/null 2>&1 &
